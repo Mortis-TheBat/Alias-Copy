c9a24a8efa8263d480241d5fcd506a4da00bb8c5
Udit Mahajan <68887022+Mortis-TheBat@users.noreply.github.com>
2021-01-10 14:26:55 +0000

    Add files via upload

---
+++ empty.sh
@@ -0,0 +1,12 @@
+alias try='sudo openvpn /home/nani/tryhackmeVPN.ovpn'
+alias des='cd /home/nani/Desktop'
+alias home='cd /home/nani'
+alias msf='sudo service postgresql start && sudo msfdb init && msfconsole'
+alias als='nano ~/.bash_aliases'
+# make grep output colorful 
+alias clr='grep --color=auto'
+alias upgrade='sudo parrot-upgrade'
+
+#python3 -m venv env_name then -
+alias work="cd ~/env_work/work/bin && source activate"
+
